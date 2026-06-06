b0425b147533d22cd7daa49a0e8a0239a5314418
Mohammed Mbarak <mohammedmbarak77@gmail.com>
2026-06-06 22:45:34 +0300

    add netlify proxy function

--- src/services/axios.js
+++ src/services/axios.js
@@ -1,10 +1,14 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL, // MUST be https://api.weather-ai.co/v1
-  headers: {
-    Authorization: `Bearer ${import.meta.env.VITE_WEATHER_AI_API_KEY}`,
-  },
+  baseURL: import.meta.env.PROD
+    ? "/api"
+    : import.meta.env.VITE_API_BASE_URL,
+  headers: import.meta.env.PROD
+    ? {}
+    : {
+        Authorization: `Bearer ${import.meta.env.VITE_WEATHER_AI_API_KEY}`,
+      },
 });
 
 export default api;
